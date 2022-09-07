dacda17452cb52ba3e1d03f5be65d435af18addc
nico.f20 <nico.f20@P-0G-0B005-0394>
2022-09-07 12:52:56 +0200

    ud

---
+++ 5DatentypVerzweigungen/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Bitte geben sie die erste zahl ein:");
+string mich1 = Console.ReadLine();
+Console.WriteLine("Bitte geben sie die zweite zahl ein:");
+string mich2 = Console.ReadLine();
+
+int michint1 = Convert.ToInt32(mich1); 
+int michint2 = Convert.ToInt32(mich2);
+
+if (michint1 > michint2)
+
+    Console.WriteLine(michint2);
+else
+    Console.WriteLine(michint1);
